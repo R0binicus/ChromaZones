aa5ca5b1e82fd6f7637bcc70143af6c3bf6f5f52
Maddi <maddi.fitzmaurice@gmail.com>
2023-11-18 00:26:33 +1100

    UI_03
    - Created title screen with basic button functionality

---
+++ Gradient Stealth Game/Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject _mainMenu;
+    [SerializeField] GameObject _instructions;
+
+    [SerializeField]
+    float quitDelay = 1f;
+
+    private void Awake()
+    {
+        // If using the Unity editor or development build, enable debug logs
+        Debug.unityLogger.logEnabled = Debug.isDebugBuild;
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void InstructionsButton()
+    {
+        _instructions.SetActive(true);
+        _mainMenu.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+		Application.Quit(), quitDelay;
+#endif
+    }
+
+    public void BackToMenuButton()
+    {
+        _instructions.SetActive(false);
+        _mainMenu.SetActive(true);
+    }
+}
