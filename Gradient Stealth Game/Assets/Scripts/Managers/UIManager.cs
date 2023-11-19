0fc5e33ee5c59e31f073a5fa8aad6452d62799cc
Maddi <maddi.fitzmaurice@gmail.com>
2023-11-19 14:58:05 +1100

    Game UI Screen Fix
    - Added Next Level Button
    - Made sure Next Level Button does not show on final level

--- Gradient Stealth Game/Assets/Scripts/Managers/UIManager.cs
+++ Gradient Stealth Game/Assets/Scripts/Managers/UIManager.cs
@@ -6,14 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    // UI Panels
     [SerializeField] GameObject _gameCanvas;
     [SerializeField] GameObject _winPanel;
     [SerializeField] GameObject _losePanel;
+    [SerializeField] GameObject _nextLevelButton;
+
+    // Scene Tracker
+    int _currentSceneIndex;
+    int _numOfScenes;
 
     private void Awake()
     {
         // Deactivate UI at start of game
         DeactivateUI();
+
+        // Get current Scene index and total number of scenes in game
+        _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        _numOfScenes = SceneManager.sceneCountInBuildSettings;
     }
 
     private void OnEnable()
@@ -26,6 +36,14 @@ public class UIManager : MonoBehaviour
     {
         EventManager.EventUnsubscribe(EventType.LOSE, ShowLosePanel);
         EventManager.EventUnsubscribe(EventType.WIN, ShowWinPanel);
+    }
+
+    public void NextLevel()
+    {
+        if (_currentSceneIndex < _numOfScenes - 1)
+        {
+            SceneManager.LoadScene(_currentSceneIndex + 1);
+        }
     }
 
     // Button callback to replay game
@@ -46,16 +64,28 @@ public class UIManager : MonoBehaviour
         _gameCanvas.SetActive(false);
         _losePanel.SetActive(false);
         _winPanel.SetActive(false);
+        _nextLevelButton.SetActive(false);
     }
 
     private void ShowLosePanel(object data)
     {
+        _nextLevelButton.SetActive(false);
         _gameCanvas.SetActive(true);
         _losePanel.SetActive(true);
     }
 
     private void ShowWinPanel(object data)
     {
+        // If last level, do not show next level button
+        if (_currentSceneIndex == _numOfScenes - 1)
+        {
+            _nextLevelButton.SetActive(false);
+        }
+        else
+        {
+            _nextLevelButton.SetActive(true);
+        }
+
         _gameCanvas.SetActive(true);
         _winPanel.SetActive(true);
     }
