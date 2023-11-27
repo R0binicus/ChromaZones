48c7e26c7e7c8d5f64c1f0ff4c294a1dcf4e82ce
R0binicus <102570571+R0binicus@users.noreply.github.com>
2023-11-27 21:44:08 +1100

    Systems #30g
    - Input manager player movement minor improvement

--- Gradient Stealth Game/Assets/Scripts/Managers/InputManager.cs
+++ Gradient Stealth Game/Assets/Scripts/Managers/InputManager.cs
@@ -10,8 +10,6 @@ public class InputManager : MonoBehaviour, InputActions.IGameplayActions
         _inputs = new InputActions();
         _inputs.Gameplay.SetCallbacks(this);
         _inputs.Gameplay.Enable();
-        _inputs.Gameplay.Move.performed += MovingTrue;
-        _inputs.Gameplay.Move.canceled += MovingFalse;
 
         EventManager.EventInitialise(EventType.PAUSE_TOGGLE);
         EventManager.EventInitialise(EventType.PLAYER_MOVE_BOOL);
@@ -30,15 +28,15 @@ public class InputManager : MonoBehaviour, InputActions.IGameplayActions
     public void OnMove(InputAction.CallbackContext context)
     {
         EventManager.EventTrigger(EventType.PLAYER_MOVE_VECT2D, _inputs.Gameplay.Move.ReadValue<Vector2>());
-    }
-
-    private void MovingTrue(InputAction.CallbackContext context)
-    {
-        EventManager.EventTrigger(EventType.PLAYER_MOVE_BOOL, true);
-    }
-
-    private void MovingFalse(InputAction.CallbackContext context)
-    {
-        EventManager.EventTrigger(EventType.PLAYER_MOVE_BOOL, false);
+        if (context.performed)
+        {
+            Debug.Log("MovingTrue");
+            EventManager.EventTrigger(EventType.PLAYER_MOVE_BOOL, true);
+        }
+        else
+        {
+            Debug.Log("MovingFalse");
+           EventManager.EventTrigger(EventType.PLAYER_MOVE_BOOL, false); 
+        }
     }
 }
