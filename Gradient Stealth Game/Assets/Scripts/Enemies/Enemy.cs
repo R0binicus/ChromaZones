7b27b7452220aa3e0efd06ac71d4d966ef05b083
R0binicus <102570571+R0binicus@users.noreply.github.com>
2023-12-28 23:13:13 +1100

    Zali Playtesting Fix 6
    - Replaced player death fix needs with one that may potentially fix several other edge case related issues

--- Gradient Stealth Game/Assets/Scripts/Enemies/Enemy.cs
+++ Gradient Stealth Game/Assets/Scripts/Enemies/Enemy.cs
@@ -198,10 +198,10 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (_dontStartYet >= 5)
-        {
+        //if (_dontStartYet >= 5)
+        //{
             StateMachine.CurrentState.OnCollisionEnter2D(collision);
-        }
+        //}
     }
 
     public bool PlayerSpotted()
