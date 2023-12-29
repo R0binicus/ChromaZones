db9686dad61c9626c3eb3e7a8a2a628073bdbcd3
Maddi <maddi.fitzmaurice@gmail.com>
2023-12-29 19:40:20 +1100

    UI Fixes 01a
    - Reimplemented lost load/save code from UI #55c

--- Gradient Stealth Game/Assets/Scripts/Managers/SceneSystemManager.cs
+++ Gradient Stealth Game/Assets/Scripts/Managers/SceneSystemManager.cs
@@ -45,6 +45,7 @@ public class SceneSystemManager : MonoBehaviour
         EventManager.EventSubscribe(EventType.LEVEL_SELECTED, LevelSelected);
         EventManager.EventSubscribe(EventType.NEXT_LEVEL, NextLevelHandler);
         EventManager.EventSubscribe(EventType.RESTART_LEVEL, RestartLevelHandler);
+        EventManager.EventSubscribe(EventType.WIN, WinHandler);
         EventManager.EventSubscribe(EventType.QUIT_LEVEL, QuitLevelHandler);
         EventManager.EventSubscribe(EventType.QUIT_GAME, QuitGameHandler);
     }
@@ -54,6 +55,7 @@ public class SceneSystemManager : MonoBehaviour
         EventManager.EventUnsubscribe(EventType.LEVEL_SELECTED, LevelSelected);
         EventManager.EventUnsubscribe(EventType.NEXT_LEVEL, NextLevelHandler);
         EventManager.EventUnsubscribe(EventType.RESTART_LEVEL, RestartLevelHandler);
+        EventManager.EventUnsubscribe(EventType.WIN, WinHandler);
         EventManager.EventUnsubscribe(EventType.QUIT_LEVEL, QuitLevelHandler);
         EventManager.EventUnsubscribe(EventType.QUIT_GAME, QuitGameHandler);
     }
@@ -86,13 +88,22 @@ public class SceneSystemManager : MonoBehaviour
     }
 
     #region Game UI Response
+    // Listens for when game is won
+    public void WinHandler(object data)
+    {
+        // Check if last level
+        if (_currentLevel.buildIndex < _numOfScenes - 1)
+        {
+            EventManager.EventTrigger(EventType.SAVE_GAME, _currentLevel.buildIndex + 1);
+        }
+    }
+
     // Listens for when NextLevelButton is pressed
     public void NextLevelHandler(object data)
     {
         // Check if last level
         if (_currentLevel.buildIndex < _numOfScenes - 1)
         {
-            EventManager.EventTrigger(EventType.SAVE_GAME, _currentLevel.buildIndex + 1);
             StartCoroutine(LevelChanger(_currentLevel.buildIndex, _currentLevel.buildIndex + 1));
         }
     }
