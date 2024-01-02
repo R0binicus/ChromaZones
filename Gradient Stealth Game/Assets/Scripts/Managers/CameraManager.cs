82b7ec77003ed16df3133857f6323c04d8df2571
Maddi <maddi.fitzmaurice@gmail.com>
2024-01-02 15:42:06 +1100

    Ukiyo Playtesting 17
    - Serialized orthographic sizes

--- Gradient Stealth Game/Assets/Scripts/Managers/CameraManager.cs
+++ Gradient Stealth Game/Assets/Scripts/Managers/CameraManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     private Camera _mainCamera;
+    [SerializeField] float _orthoSize169 = 5f;
+    [SerializeField] float _orthoSize1610 = 5.9f;
+    [SerializeField] float _orthoSize32 = 6.2f;
 
     void Awake()
     {
@@ -13,16 +16,16 @@ public class CameraManager : MonoBehaviour
         switch (_mainCamera.aspect)
         {
             case (16f / 9f): 
-                _mainCamera.orthographicSize = 5f;
+                _mainCamera.orthographicSize = _orthoSize169;
                 break;
             case (16f / 10f):
-                _mainCamera.orthographicSize = 5.9f;
+                _mainCamera.orthographicSize = _orthoSize1610;
                 break;
             case (3f / 2f):
-                _mainCamera.orthographicSize = 6.2f;
+                _mainCamera.orthographicSize = _orthoSize32;
                 break;
             default:
-                _mainCamera.orthographicSize = 5f;
+                _mainCamera.orthographicSize = _orthoSize169;
                 break;
         }
     }
